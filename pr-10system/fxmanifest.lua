13079b916826b5a8af4a19cedb98aeb4dc585b2a
lIAM231 <31771817+lIAM231@users.noreply.github.com>
2021-07-12 17:31:10 +0300

    Updateing some dumb shit
    Updating some dumb shit

--- pr-10system/fxmanifest.lua
+++ pr-10system/fxmanifest.lua
@@ -1,3 +1,11 @@
+
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'LiamInChains#9999  - ilAn#7021 '
+description 'Partizan Whitelist 10 shit system'
+version '1.0.0'
+
 shared_scripts {
     'config/*'
 }
@@ -19,4 +27,3 @@ files {
     'ui/main.js',
     'ui/style.css'
 }
-Author 'Liam01#3776'
